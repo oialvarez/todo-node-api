3159153d9cf63f816af6e14b2f5c9bb078f92dbd
oialvarez <oialvarez@gmail.com>
2018-08-06 04:58:30 -0400

    agregando router todo

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -19,8 +16,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+var indexRouter = require('./routes/index');
 app.use('/', indexRouter);
+
+var usersRouter = require('./routes/users');
 app.use('/users', usersRouter);
+
+var usersRouter = require('./routes/todos');
+app.use('/todos', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
